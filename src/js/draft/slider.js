3a0fa799e7e06aeddd7c6b26927223f63a970212
Eugene <dijjekas06@gmail.com>
2021-03-07 20:18:02 +0200

    Main page w\out responsive

---
+++ src/js/draft/slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $(".hero__slider").slick({
+    arrows: true,
+    dots: true,
+    // responsive: [
+    //   {
+    //     breakpoint: 930,
+    //     settings: {
+    //       centerMode: true,
+    //       centerPadding: '10px',
+    //       slidesToShow: 3,
+    //       slidesToScroll: 1
+    //     }
+    //   }
+    // ]
+  });
+});
